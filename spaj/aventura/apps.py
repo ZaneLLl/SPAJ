ec50057c471ac8b09c13b75fc87bc1e1695c05f7
ZANELLI <p3dr0h3nrique2003@gmail.com>
2022-11-17 13:47:41 -0300

    Ultima atualização 17/11

---
+++ spaj/aventura/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class AventuraConfig(AppConfig):
+    #default_auto_field = 'spaj.aventura.models'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'aventura'
